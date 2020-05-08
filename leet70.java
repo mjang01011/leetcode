d4e60b4973bd079a3e26c44bad61e1209513456c
mjang01011 <53363142+mjang01011@users.noreply.github.com>
2020-05-08 20:43:03 +0900

    Add files via upload

---
+++ leet70.java
@@ -0,0 +1,33 @@
+/*
+ID: mjang
+LANG: JAVA
+TASK: leet70
+*/
+
+//DP Solution
+
+import java.util.*;
+import java.io.*;
+
+public class leet70{
+
+	public static int climbStairs(int n){
+		int[] memo = new int[n + 1];
+		return calc(0, n, memo);
+	}
+
+	public static int calc(int i, int n, int[] memo){
+		if(i > n)
+			return 0;
+		if(i == n)
+			return 1;
+		if(memo[i] > 0)
+			return memo[i];
+		memo[i] = calc(i + 1, n, memo) + calc(i + 2, n, memo);
+		return memo[i];
+	}
+
+	public static void main(String[] args) throws IOException{
+		System.out.println(climbStairs(45));
+	}
+}
